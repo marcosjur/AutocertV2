a32cb6f0a574e9b7cdba115723165dd773e3af6c
Marcos Barboza <marcosjr861@gmail.com>
2022-06-17 10:04:42 -0300

    New app

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+app.route('/', methods=['GET','POST'])
+def home():
+    return "<p>Home</p>"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
