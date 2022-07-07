e8b757f2a8267521a58495efe5025ed120f83736
Marcos Barboza <marcosjr861@gmail.com>
2022-07-07 14:46:39 -0300

    redirect fix - render

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def autocert_form_handler():
     }
     try:
         Certificate(username=name, useremail=email, usercourse=course).writing_certificate()
-        return redirect('https://prd1.azurewebsites.net/success', code=200)
+        return render_template("success.html")
     except Exception as e:
         return Response(status=500, mimetype='application/json')
     
