74833123e1eb806c18325ee47649437433287678
Marcos Barboza <marcosjr861@gmail.com>
2022-07-08 01:20:46 -0300

    FIX Troca do python-dotenv pelo os.getenv

--- projects/send_email.py
+++ projects/send_email.py
@@ -2,10 +2,7 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from pathlib import Path
-from dotenv import load_dotenv
 import os
-
-load_dotenv()
 
 # enviando email
 class SendMail:
@@ -14,8 +11,8 @@ class SendMail:
         self.cert = cert
         self.anexo = Path(self.cert)
         self.server = smtplib.SMTP('smtp.gmail.com', 587)
-        self.email = os.environ['AUTOCERT_EMAIL_FROM']
-        self.key = str(os.environ['AUTOCERT_TOKEN_EMAIL'])
+        self.email = os.getenv('AUTOCERT_EMAIL_FROM')
+        self.key = str(os.getenv('AUTOCERT_TOKEN_EMAIL'))
         self.msg = MIMEMultipart()
         self.email_destiny = receiver
         
@@ -28,14 +25,13 @@ class SendMail:
         self.server.login(self.email, self.key)
 
         # Mensage configuration
-        self.msg["From"] = str(os.environ['AUTOCERT_EMAIL_FROM'])
+        self.msg["From"] = str(os.getenv('AUTOCERT_EMAIL_FROM'))
         self.msg["to"] = self.email_destiny
-        self.msg["Subject"] = str(os.environ['AUTOCERT_EMAIL_SUBJECT'])
+        self.msg["Subject"] = str(os.getenv('AUTOCERT_EMAIL_SUBJECT'))
         self.msg["Bcc"] = self.email_destiny
 
         # Send email
         self.msg.attach(MIMEText(self.cert, 'html'))
-        print("FOI")
         
         # Drop the server
         self.server.send_message(self.msg)
